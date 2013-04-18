28b64313a943730c057c9344182d967ea889e23e
JuanJose <jmorale1@trinity.edu>
2013-04-18 15:32:29 -0500

    Tenant Functions Tested
    tenant functions have been tested and are working properly

--- Keystone_Gem/test/test_auth.rb
+++ Keystone_Gem/test/test_auth.rb
@@ -5,32 +5,39 @@ require 'test_parameters'
 #testing the auth function
 
 class TestAuth < Test::Unit::TestCase
-	def test_auth_server
-		c = Keystone.new($admin, $adminPass, "WRONG_SERVER", $serverPort1, $serverPort2)
+	def test_1_auth_server
+		c = Keystone.new($admin,$adminPass,"WRONG_SERVER",$serverPort1,$serverPort2)
 		expected = c.auth($tenant)
 		assert_equal expected, false
 	end
 	
-	def test_auth_password
-		c = Keystone.new($admin, "WRONG", $serverURL, $serverPort1, $serverPort2)
+	def test_2_auth_password
+		c = Keystone.new($admin,"WRONG",$serverURL,$serverPort1,$serverPort2)
 		expected = c.auth($tenant)
 		assert_equal expected, false
 	end
 	
-	def test_auth_username
-		c = Keystone.new("WRONG", $adminPass, $serverURL,$serverPort1,$serverPort2)
+	def test_3_auth_username
+		c = Keystone.new("WRONG",$adminPass,$serverURL,$serverPort1,$serverPort2)
 		expected = c.auth($tenant)
 		assert_equal expected, false
 	end
 	
-	def test_auth_tenant
-		c = Keystone.new($admin, $adminPass, $serverURL, $serverPort1, $serverPort2)
+	def test_4_auth_tenant
+		c = Keystone.new($admin,$adminPass,$serverURL,$serverPort1,$serverPort2)
 		expected = c.auth("WRONG")
 		assert_equal expected, false
 	end
 	
-	def test_auth_correct
-		c = Keystone.new($admin, $adminPass, $serverURL, $serverPort1, $serverPort2)
+	def test_5_auth_different_tenant
+		c = Keystone.new("test01u",$adminPass,$serverURL,$serverPort1,$serverPort2)
+		c.auth($tenant)
+		expected = c.auth($tenant)
+		assert_equal expected, false
+	end
+	
+	def test_6_auth_correct
+		c = Keystone.new($admin,$adminPass,$serverURL,$serverPort1,$serverPort2)
 		expected = c.auth($tenant)
 		assert_equal expected, true
 	end
--- Keystone_Gem/test/test_tenant.rb
+++ Keystone_Gem/test/test_tenant.rb
@@ -5,29 +5,91 @@ require 'test_parameters.rb'
 
 #testing the auth function
 
-
 class TestTenant < Test::Unit::TestCase
 	
-	#tenant_list
+#tenant_list
 	def test_tenant_list	
-		c = Keystone.new($admin, $adminPass, $serverURL, $serverPort1, $serverPort2)
+		c = Keystone.new($admin,$adminPass,$serverURL,$serverPort1,$serverPort2)
 		c.auth($tenant)
 		expected = c.tenant_list
 		assert_not_nil(expected)
 	end
 	
+	def tenant_list_different_tenant
+		d = Keystone.new("test01u",$adminPass,$serverURL,$serverPort1,$serverPort2)
+		d.auth("test01")
+		expected = d.tenant_list
+		assert_not_nil(expected)
+	end
+	
 	def test_tenant_list_fail_to_authenticate
-		d = Keystone.new($admin, $adminPass, $serverURL, $serverPort1, $serverPort2)
+		d = Keystone.new($admin,"WRONG_PASSWORD",$serverURL,$serverPort1,$serverPort2)
 		d.auth($tenant)
 		expected = d.tenant_list
 		assert_nil(expected)
 	end
 	
-	def tenant_list_empty
-		d = Keystone.new($admin, $adminPass, $serverURL, $serverPort1, $serverPort2)
-		d.auth($tenant)
-		expected = d.tenant_list
-		assert_not_nill(expected)
+	def test_tenant_create
+		c = Keystone.new($admin,$adminPass,$serverURL,$serverPort1,$serverPort2)
+		c.auth($tenant)
+		expected = c.tenant_create("testing","testing")
+		assert_not_nil(expected)
 	end
 
+	def test_tenant_get_tenant_in_database
+		c = Keystone.new($admin,$adminPass,$serverURL,$serverPort1,$serverPort2)
+		c.auth($tenant)
+		id = tenant_id_search(c, "testing")
+		expected = c.tenant_get(id)
+		assert_not_nil(expected)
+	end
+	
+	def test_tenant_get_tenant_not_in_database
+		c = Keystone.new($admin,$adminPass,$serverURL,$serverPort1,$serverPort2)
+		c.auth($tenant)
+		expected = c.tenant_get("TENANT_NOT_IN_DATABASE!!")
+		assert_nil(expected)
+	end
+	
+	
+	def test_tenant_update
+		c = Keystone.new($admin,$adminPass,$serverURL,$serverPort1,$serverPort2)
+		c.auth($tenant)
+		id = tenant_id_search(c, "testing")
+		
+		if id != 0
+			expected = c.tenant_update("change_the_testing_temp", "change a test", id)
+		else
+			excpected = nil
+		end
+		assert_not_nil(expected)
+	end
+	
+	def test_tenant_delete
+		c = Keystone.new($admin,$adminPass,$serverURL,$serverPort1,$serverPort2)
+		c.auth($tenant)
+		id = tenant_id_search(c, "change_the_testing_temp")
+		
+		if id != 0
+			expected = c.tenant_delete(id)
+		else
+			excpected = false
+		end
+		assert_equal expected, true
+	end
+
+end
+
+def tenant_id_search(keystone_object, name)
+	
+		tenants = keystone_object.tenant_list
+		tenant_list = tenants["tenants"]
+		
+		for t in tenant_list
+			if (t.has_value? name)
+				return t["id"]
+			end
+		end
+		
+		return 0
 end
