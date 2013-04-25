8499563a08ee079df9c9751ef8cdf473769105b7
Aaron Lawson <alawson2@trinity.edu>
2013-04-25 10:27:09 -0500

    fixed test_user to deal with new tenant_create return of tuples

--- Keystone_Gem/test/test_user.rb
+++ Keystone_Gem/test/test_user.rb
@@ -11,13 +11,13 @@ class TestUser < Test::Unit::TestCase
     c.auth($tenant)
     withoutNewUser = c.user_list
     newTenant = c.tenant_create('test_user_list', 'mock tenant')
-    newUser= c.user_create('test_user_list','test_user_list@com.com','secret', newTenant['tenant']['id'])
+    newUser= c.user_create('test_user_list','test_user_list@com.com','secret', newTenant[0]['tenant']['id'])
     withNewUser = c.user_list
 
     assert(true, withoutNewUser==withNewUser) #makes sure that the list does in fact change
 
     c.user_delete(newUser['user']['id'])
-    c.tenant_delete(newTenant['tenant']['id'])
+    c.tenant_delete(newTenant[0]['tenant']['id'])
   end
 
   def test_user_create
@@ -26,21 +26,23 @@ class TestUser < Test::Unit::TestCase
     c.auth($tenant)
     withoutNew = c.user_list['users']
     newTenant = c.tenant_create("test_user_create", "")
-    newUser = c.user_create("test_user_create", "email", "password", newTenant['tenant']['id'])
+    newUser = c.user_create("test_user_create", "email", "password", newTenant[0]['tenant']['id'])
     withNew = c.user_list['users']
 
     difference = withNew.size - withoutNew.size
     assert_equal(1, difference)
 
     c.user_delete(newUser['user']['id'])
-    c.tenant_delete(newTenant['tenant']['id'])
+    c.tenant_delete(newTenant[0]['tenant']['id'])
   end
 
+=begin
   def test_user_create_unauthorized
     #this should not allow creation of users
     c = Keystone.new($admin, $adminPass, $serverURL, $serverPort1, $serverPort2)
     c.auth($tenant)
   end
+=end
 
   def test_user_create_invalid_tenant
     #this should not allow creation of users
@@ -59,7 +61,7 @@ class TestUser < Test::Unit::TestCase
 
     withoutUser = c.user_list
     newTenant = c.tenant_create("test_user_delete", "")
-    newUser = c.user_create("test_user_delete", "email", "password", newTenant['tenant']['id'])
+    newUser = c.user_create("test_user_delete", "email", "password", newTenant[0]['tenant']['id'])
     withUser = c.user_list
 
     difference = withUser['users'].size - withoutUser['users'].size
@@ -68,17 +70,19 @@ class TestUser < Test::Unit::TestCase
     userDeletedList = c.user_list
     difference = withoutUser['users'].size - userDeletedList['users'].size
     assert_equal(0, difference)
-    c.tenant_delete(newTenant['tenant']['id'])
+    c.tenant_delete(newTenant[0]['tenant']['id'])
 
 
   end
 
+=begin
   def test_user_delete_unauthorized
     #this should not allow deletion of users
     c = Keystone.new($admin, $adminPass, $serverURL, $serverPort1, $serverPort2)
     c.auth($tenant)
 
   end
+=end
 
   def test_user_delete_user_doesnt_exist
     #this should not allow deletion of users
@@ -93,12 +97,12 @@ class TestUser < Test::Unit::TestCase
     a = Keystone.new($admin, $adminPass, $serverURL, $serverPort1, $serverPort2)
     a.auth($tenant)
     newTenant = a.tenant_create("user_get_tenant", "for testing purposes")
-    newUser = a.user_create("user_get", "secret", "user_get_email", newTenant['tenant']['id'])
+    newUser = a.user_create("user_get", "secret", "user_get_email", newTenant[0]['tenant']['id'])
 
     assert_equal(newUser["user"]["id"],a.user_get(newUser['user']['id'])['user']['id'])
 
     a.user_delete(newUser["user"]["id"])
-    a.tenant_delete(newTenant['tenant']['id'])
+    a.tenant_delete(newTenant[0]['tenant']['id'])
   end
 
   def test_user_get_doesnt_exist
@@ -116,15 +120,15 @@ class TestUser < Test::Unit::TestCase
     a = Keystone.new($admin, $adminPass, $serverURL, $serverPort1, $serverPort2)
     a.auth($tenant)
     newTenant = a.tenant_create("test_password_update", "for testing purposes")
-    newUser = a.user_create("test_password_update", "password@yahoo", "secret", newTenant['tenant']['id'])
+    newUser = a.user_create("test_password_update", "password@yahoo", "secret", newTenant[0]['tenant']['id'])
 
     a.user_password_update(newUser['user']['id'], "compromised")
     b = Keystone.new("test_password_update", "compromised", $serverURL,$serverPort1,$serverPort2)
 
-    assert_equal(b.auth(newTenant['tenant']['name']),true)
+    assert_equal(b.auth(newTenant[0]['tenant']['name'])[0],true)
 
     a.user_delete(newUser['user']['id'])
-    a.tenant_delete(newTenant['tenant']['id'])
+    a.tenant_delete(newTenant[0]['tenant']['id'])
   end
 
 =begin
