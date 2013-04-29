7a0b8b2ffb110dd87df0456400355c939faf4f31
Aaron Lawson <alawson2@trinity.edu>
2013-04-28 22:05:13 -0500

    parameters back to our original server

--- Keystone_Gem/test/test_parameters.rb
+++ Keystone_Gem/test/test_parameters.rb
@@ -1,9 +1,7 @@
 
 $admin = "admin"
-$adminPass = "trinitytu"
-$serverURL = "cloud.cs.trinity.edu"
-#http://198.61.199.47
+$adminPass = "password"
+$serverURL = "http://198.61.199.47"
 $serverPort1 = "5000"
 $serverPort2 = "35357"
-$tenant = "demo"
-#tenant1
+$tenant = "tenant1"
