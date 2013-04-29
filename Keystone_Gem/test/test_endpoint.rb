bf70fe54b5eb03c703c4cb40f396d847cd4eb3c4
NuRelic <bnowlin1@trinity.edu>
2013-04-28 22:22:09 -0500

    Update test_endpoint.rb
    dollar signs

--- Keystone_Gem/test/test_endpoint.rb
+++ Keystone_Gem/test/test_endpoint.rb
@@ -11,11 +11,11 @@ class TestRole < Test::Unit::TestCase
 	
 	begin #endpoint_list
 		def test_endpoint_list
-			expected = aut.endpoint_list
+			expected = $aut.endpoint_list
 			assert(expected[0])
 		end
 		def test_endpoint_list_fail_to_authenticate
-			expected = fai.endpoint_list
+			expected = $fai.endpoint_list
 			assert(!expected[0])
 		end
 	end #endpoint_list
@@ -23,24 +23,24 @@ class TestRole < Test::Unit::TestCase
 	begin #endpoint_delete
 		#NOTE: Utilizes get and create to clean up data
 		def test_endpoint_delete
-			servID = aut.service_list[1]["OS-KSADM:services"][0]["id"] # retrieve service ID
-			expected = aut.endpoint_create("dummyDebug",servID,"1","1","1")
+			servID = $aut.service_list[1]["OS-KSADM:services"][0]["id"] # retrieve service ID
+			expected = $aut.endpoint_create("dummyDebug",servID,"1","1","1")
 			assert(expected[0])
-			deleted = aut.endpoint_delete(expected[1]["endpoint"]["id"])
+			deleted = $aut.endpoint_delete(expected[1]["endpoint"]["id"])
 			assert(deleted[0])
-			expected = aut.endpoint_get(expected[1]["endpoint"]["id"])
+			expected = $aut.endpoint_get(expected[1]["endpoint"]["id"])
 			assert(!expected[0])
 		end	
 		
 		def test_endpoint_delete_fail_to_authenticate #NOTE uses authenticated endpoint list
-			endptID = aut.endpoint_list[1]["endpoints"][0]["id"] # retrieve endpoint ID
-			expected = fai.endpoint_delete(endptID)
+			endptID = $aut.endpoint_list[1]["endpoints"][0]["id"] # retrieve endpoint ID
+			expected = $fai.endpoint_delete(endptID)
 			assert(!expected[0])
 		end
 		
 		def test_endpoint_delete_does_not_exist
 			id = "00000000000000000000000000000000" # Could try to use a non-hex string to ensure, but won't
-			expected = aut.endpoint_delete(id)
+			expected = $aut.endpoint_delete(id)
 			assert(!expected[0])
 		end
 	end #endpoint_delete
@@ -48,24 +48,24 @@ class TestRole < Test::Unit::TestCase
 	begin #endpoint_get
 		#NOTE: Utilizes delete and create to clean up data
 		def test_endpoint_get
-			servID = aut.service_list[1]["OS-KSADM:services"][0]["id"] # retrieve service ID
-			expected = aut.endpoint_create("dummyDebug",servID,"1","1","1")
+			servID = $aut.service_list[1]["OS-KSADM:services"][0]["id"] # retrieve service ID
+			expected = $aut.endpoint_create("dummyDebug",servID,"1","1","1")
 			assert(expected[0])
-			deleted = aut.endpoint_delete(expected[1]["endpoint"]["id"])
+			deleted = $aut.endpoint_delete(expected[1]["endpoint"]["id"])
 			assert(deleted[0])
-			expected = aut.endpoint_get(expected[1]["endpoint"]["id"])
+			expected = $aut.endpoint_get(expected[1]["endpoint"]["id"])
 			assert(!expected[0])
 		end
 		
 		def test_endpoint_get_fail_to_authenticate #NOTE uses authenticated endpoint list
-			endptID = aut.endpoint_list[1]["endpoints"][0]["id"] # retrieve endpoint ID
-			expected = fai.endpoint_get(endptID)
+			endptID = $aut.endpoint_list[1]["endpoints"][0]["id"] # retrieve endpoint ID
+			expected = $fai.endpoint_get(endptID)
 			assert(!expected[0])
 		end
 		
 		def test_endpoint_get_does_not_exist
 			id = "00000000000000000000000000000000" # Could try to use a non-hex string to ensure, but won't
-			expected = aut.endpoint_get(id)
+			expected = $aut.endpoint_get(id)
 			assert(!expected[0])
 		end
 	end #endpoint_get
@@ -73,18 +73,18 @@ class TestRole < Test::Unit::TestCase
 	begin #endpoint_create 
 		#NOTE: Utilizes delete and get to clean up data
 		def test_endpoint_create
-			servID = aut.service_list[1]["OS-KSADM:services"][0]["id"] # retrieve service ID
-			expected = aut.endpoint_create("dummyDebug",servID,"1","1","1")
+			servID = $aut.service_list[1]["OS-KSADM:services"][0]["id"] # retrieve service ID
+			expected = $aut.endpoint_create("dummyDebug",servID,"1","1","1")
 			assert(expected[0])
-			deleted = aut.endpoint_delete(expected[1]["endpoint"]["id"])
+			deleted = $aut.endpoint_delete(expected[1]["endpoint"]["id"])
 			assert(deleted[0])
-			expected = aut.endpoint_get(expected[1]["endpoint"]["id"])
+			expected = $aut.endpoint_get(expected[1]["endpoint"]["id"])
 			assert(!expected[0])
 		end	
 
 		def test_endpoint_create_fail_to_authenticate #NOTE uses authenticated service, failed endpoint
-			servID = aut.service_list[1]["OS-KSADM:services"][0]["id"] # retrieve service ID
-			expected = fai.endpoint_create("dummyDebug",servID,"1","1","1")
+			servID = $aut.service_list[1]["OS-KSADM:services"][0]["id"] # retrieve service ID
+			expected = $fai.endpoint_create("dummyDebug",servID,"1","1","1")
 			assert(!expected[0])
 		end
 	end #endpoint_create	
