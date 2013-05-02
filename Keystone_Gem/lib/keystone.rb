9ecb2e0d43fee8b7174f3024b9355cfd1f831edc
JuanJose <jmorale1@trinity.edu>
2013-05-02 15:21:23 -0500

    thundercats are go
    user_update, user_role_add/remove/list done

--- Keystone_Gem/lib/keystone.rb
+++ Keystone_Gem/lib/keystone.rb
@@ -131,48 +131,103 @@ class Keystone
     end
 
 #_______________________________________________________________________________________________     
+    
     def user_update(email, id)
-=begin
-      name_blank = (name.nil?) or (name.empty?)
-      email_blank = (email.nil?) or (email.empty?)
-      no_user = (user_get(id).nil?) or (user_get(id).empty?)
-
-      if(name_blank && email_blank)
-        comment = "No update occured: blank username and email."
-        return [nil,comment]
-      elsif(no_user)
-        comment = "User update failed: no user for id #{id}."
-        return [nil,comment]
-      elsif(name_blank)   
-        comment = "No username update occured."
-        user = {"user" => {"email" => email, "enabled" => true}}
-      elsif(email_blank)
-        comment = "No email update occured."
-        user = {"user" => {"name" => name, "enabled" => true}}
-      else
-        comment = "User successfully updated: #{name}, #{email}"
-        user = {"user" => {"name" => name, "email" => email, "enabled" => true}}
-      end
-=end
-      user = {"user" => {"email" => email, "enabled" => true}}
+      user = {"user" => {"id" => id, "email" => email}}
       json_string = JSON.generate(user)
-      post_call   = Curl::Easy.http_put("#{@ip_address}:#{@port_2}/v2.0/users/#{id}/OS-KSADM/email", json_string
-      ) do |curl|
-        curl.headers['x-auth-token'] = @token
-        curl.headers['Content-Type'] = 'application/json'
-      end
-      parsed_json = JSON.parse(post_call.body_str)
+      new_url = "#{@ip_address}:#{@port_2}/v2.0/users/#{id}", flag = true
+      begin
+        put_call = Curl::Easy.http_put(new_url, json_string
+        ) do |curl|
+          curl.headers['x-auth-token'] = @token
+          curl.headers['Content-Type'] = 'application/json'
+        end
+        parsed_json = JSON.parse(put_call.body_str)
 
-      if (parsed_json.to_s.include? 'error')
-        error = parsed_json["error"]["message"]
-        e = error.to_s
-        return [nil, e]
-      else
-        return [parsed_json, "user updated successfully"]
+        if (parsed_json.key? 'error')
+          error = parsed_json["error"]["message"] 
+          return [flag, error.to_s]
+        else
+          return [flag, parsed_json]
+        end
+      rescue
+        error = "failed to update user -- put call failed"
+        return [flag, error]
       end
-
     end
-        #_______________________________________________________________________________________________
+#_______________________________________________________________________________________________ 
+    
+    def user_role_list(tenant_id, user_id)
+      new_url = "#{@ip_address}:#{@port_2}/v2.0/tenants/#{tenant_id}/users/#{user_id}/roles", flag = true
+      begin
+        get_call = Curl::Easy.http_get(new_url
+        ) do |curl|
+          curl.headers['x-auth-token'] = @token
+        end
+        parsed_json = JSON.parse(get_call.body_str)
+        if(parsed_json.to_s.include? 'error')
+          error = parsed_json["error"]["message"]
+          e = error.to_s
+          return [flag, e]
+        else
+          return [flag,parsed_json]
+        end
+      rescue
+        m = "get call for user_role_list failed"
+        return [flag,m]
+      end
+    end
+#_______________________________________________________________________________________________
+
+    def user_role_add(tenant_id, user_id, role_id)
+      role = {"role" => {"id" => role_id}}
+      json_string = JSON.generate(role)
+      new_url = "#{@ip_address}:#{@port_2}/v2.0/tenants/#{tenant_id}/users/#{user_id}/roles/OS-KSADM/#{role_id}", flag = true
+      begin
+        put_call = Curl::Easy.http_put(new_url, json_string
+        ) do |curl|
+          curl.headers['x-auth-token'] = @token
+          curl.headers['Content-Type'] = 'application/json'
+        end
+        parsed_json = JSON.parse(put_call.body_str)
+
+        if (parsed_json.key? 'error')
+          error = parsed_json["error"]["message"]
+          e = error.to_s
+          return [flag, e]
+        else
+          return [flag, "Role added successfully"]
+        end
+      rescue
+        m = "error with put_call in user_role_add"
+        return [flag, m]
+      end
+    end
+#_______________________________________________________________________________________________      
+    
+    def user_role_remove(tenant_id, user_id, role_id)
+      new_url = "#{@ip_address}:#{@port_2}/v2.0/tenants/#{tenant_id}/users/#{user_id}/roles/OS-KSADM/#{role_id}}", flag = true
+      begin
+        delete_call = Curl::Easy.http_delete(new_url
+        ) do |curl|
+          curl.headers['x-auth-token'] = @token
+          curl.headers['Content-Type'] = 'application/json'
+        end
+        parsed_json = JSON.parse(delete_call.body_str)
+
+        if(parsed_json.to_s.include? 'error')
+          error = parsed_json["error"]["message"]
+          e = error.to_s
+          return [flag, e]
+        else
+          return [flag, "user_role_remove successful"]
+        end
+      rescue
+        m = "error with delete_call in user_role_remove"
+        return [flag,m]
+      end
+    end
+#_______________________________________________________________________________________________
   end #USER OPS
 
   begin #SERVICE OPS
@@ -417,102 +472,6 @@ class Keystone
     end
   end #ROLE OPS
 
-
-  begin #USER_ROLE OPS
-        #_______________________________________________________________________________________________
-    def user_role_add(tenant_id, user_id, role_id)
-      nil_error = (tenant_id.nil?) or (tenant_id.empty?) or
-          (user_id.nil?)   or (user_id.empty?)   or
-          (role_id.nil?)   or (role_id.empty?)
-      role = (role_get(role_id))
-
-      if(nil_error)
-        msg = "user_role_add unsuccessful: one of the fields was empty"
-        return [false, msg]
-      elsif(tenant_get(tenant_id)[0].nil?)
-        msg = "user_role_add unsuccessful: error produced for #{tenant_id}"
-        return [false, msg]
-      elsif(user_get(user_id).hasKey? 'error')
-        msg = "user_role_add unsuccessful: error produced for #{user_id}"
-        return [false, msg]
-      elsif(role[0].nil?)
-        msg = "user_role_add unsuccessful: error produced for #{role_id}"
-        return [false, msg]
-      end
-
-      post_call = Curl::Easy.http_put("#{@ip_address}:#{@port_2}/v2.0/tenants/#{tenant_id}/users/#{user_id}/roles", role_id
-      ) do |curl|
-        curl.headers['x-auth-token'] = @token
-        curl.headers['Content-Type'] = 'application/json'
-      end
-      parsed_json = JSON.parse(post_call.body_str)
-
-      if (parsed_json.to_s.include? 'error')
-        error = parsed_json["error"]["message"]
-        e = error.to_s
-        return [false, e]
-      else
-        return [true, "Role added successfully"]
-      end
-    end
-#_______________________________________________________________________________________________      
-    def user_role_remove(tenant_id, user_id, role_id)
-
-      nil_error = (tenant_id.nil?) or (tenant_id.empty?) or
-          (user_id.nil?)   or (user_id.empty?)   or
-          (role_id.nil?)   or (role_id.empty?)
-      roles = user_role_list(tenant_id, user_id)
-
-      if(nil_error)
-        msg = "user_role_remove unsuccessful: one of the fields was empty"
-        return [false, msg]
-      elsif(tenant_get(tenant_id)[0].nil?)
-        msg = "user_role_remove unsuccessful: error produced for #{tenant_id}"
-        return [false, msg]
-      elsif(user_get(user_id).hasKey? 'error')
-        msg = "user_role_remove unsuccessful: error produced for #{user_id}"
-        return [false, msg]
-      elsif(role_get(role_id)[0].nil?)
-        msg = "user_role_remove unsuccessful: error produced for #{role_id}"
-        return [false, msg]
-      elsif(!(roles.to_is.include? roles))
-        msg = "Role is not assigned to user: #{role_id}"
-        return [false, msg]
-      end
-
-      delete_call = Curl::Easy.http_delete("#{@ip_address}:#{@port_2}/v2.0/tenants/#{tenant_id}/users/#{user_id}/roles/#{role_id}}"
-      ) do |curl|
-        curl.headers['x-auth-token'] = @token
-      end
-      parsed_json = JSON.parse(delete_call.body_str)
-
-      if(parsed_json.to_s.include? 'error')
-        error = parsed_json["error"]["message"]
-        e = error.to_s
-        return [false, e]
-      else
-        return [true, "user_role_remove successful"]
-      end
-    end
-
-#_______________________________________________________________________________________________ 
-    def user_role_list(tenant_id, user_id)
-      get_call = Curl::Easy.http_get("#{@ip_address}:#{@port_2}/v2.0/tenants/#{tenant_id}/users/#{user_id}/roles"
-      ) do |curl|
-        curl.headers['x-auth-token'] = @token
-      end
-      parsed_json = JSON.parse(get_call.body_str)
-      if(parsed_json.to_s.include? 'error')
-        error = parsed_json["error"]["message"]
-        e = error.to_s
-        return[nil, e]
-      else
-        return [parsed_json,"user_role_list success"]
-      end
-    end
-        #_______________________________________________________________________________________________
-
-  end #USER_ROLE OPS
 
   begin #ENDPOINT OPS
 
--- Keystone_Gem/test/test_user.rb
+++ Keystone_Gem/test/test_user.rb
@@ -168,5 +168,249 @@ class TestUser < Test::Unit::TestCase
     assert_equal(a.user_password_update("1337", "31337")['error']['code'],404)
   end
 
+=begin "testing user_update, user_role_add/remove/list"  
+  #_________________________________________________________________________________________________________________________________________
+  def test_user_update
+    #SETUP
+      c = Keystone.new($admin, $adminPass, $serverURL, $serverPort1, $serverPort2)
+      c.auth($tenant)
+      tenantID = c.tenant_create("test_tenant_userUpdate", "")[1]['tenant']['id']    
+      userID   = c.user_create("test_user1_userUpdate", "email", "password", tenantID)['user']['id']
+      roleID   = c.role_create("test_role1_userUpdate")[1]["role"]["id"]
+    #Setup tests
+      before_update = c.user_get(userID)   
+    #Acceptable user_update
+      new_email1    = "new_email1@test.com"
+      update1       = c.user_update(new_email1, userID)
+      after_update1 = c.user_get(userID)
+    #Make sure upser_update works    
+      success1 = update1[0]
+      success2 = !(before_update != after_update1) #!!!!!!!!!!!!!!!!
+      success3 = !(after_update1["email"] == new_email1) #!!!!!!!!!!!!!!
+    #Unacceptable user_update (nonexistent user)
+      new_email2    = "new_email2@test.com"
+      update2       = c.user_update(new_email2, "badUserID")
+      after_update2 = c.user_get(userID)
+    #Make sure this doesn't work
+      failure1 = !update2[0]  #!!!!!!!!
+      failure2 = (after_update1 != after_update2)
+      failure3 = (after_update2["email"] == new_email2)    
+    #TAKEDOWN
+      c.tenant_delete(tenantID)
+      c.user_delete(userID)
+      c.role_delete(roleID)
+    #Run asserts
+      assert(success1)
+      assert(success2)
+      assert(success3)
+      assert(!failure1)
+      assert(!failure2)
+      assert(!failure3)
+  end
+   
+  def test_user_role  
+  # These test user_role_add, user_role_remove, and user_role_list
+  # I did this because I thought it would be fun to utilize more Ruby data structures
+ 
+    #SETUP
+      c = Keystone.new($admin, $adminPass, $serverURL, $serverPort1, $serverPort2)
+      c.auth($tenant)    
+      tenantID = c.tenant_create("test_tenant_userRole", "")[1]['tenant']['id']    
+      user1ID  = c.user_create("test_user1_userRole", "email", "password", tenantID)['user']['id']
+      user2ID  = c.user_create("test_user2_userRole", "email", "password", tenantID)['user']['id']    
+      role1ID  = c.role_create("test_role1_userRole")[1]["role"]["id"]
+      role2ID  = c.role_create("test_role2_userRole")[1]["role"]["id"]
+    #Setup tests
+      test1results = setup_user_role_test1(tenantID, user1ID, role1ID)
+      test2results = setup_user_role_test2(tenantID, user1ID, role1ID, role2ID)
+      test3results = setup_user_role_test3(tenantID, user1ID, user2ID, role1ID)
+      test4results = setup_user_role_test4(tenantID, user1ID, user2ID, role1ID, role2ID)
+    #TAKEDOWN
+      c.tenant_delete(tenantID)    
+      c.user_delete(user1ID)
+      c.user_delete(user2ID)    
+      c.role_delete(role1ID)
+      c.role_delete(role2ID) 
+    #Run preliminary tests
+      results = test1results
+      if(!results[0])
+        puts "user_role_add is broken for sure"
+        return
+      elsif(!results[1])
+        puts "user_role_remove is broken for sure"
+        return
+      elsif(!results[2])
+        puts "user_role_list is broken for sure"
+        return
+      end
+    #RUN TEST 1
+      test1results.each_index do |testPassed|
+        assert(testPassed)
+      end
+    #RUN TEST 2
+      test2results.each do |testPassed|
+        assert(testPassed)
+      end    
+    #RUN TEST 3
+      test3results.each do |testPassed|
+        assert(testPassed)
+      end
+    #RUN TEST 4
+      test4results.each do |testPassed|
+        assert(testPassed)
+      end           
+  end
+    
+  def setup_user_role_test1(tenantID, userID, roleID)
+    c = Keystone.new($admin, $adminPass, $serverURL, $serverPort1, $serverPort2)
+    c.auth($tenant)  
+    results  = Array.new(7, true)   
+    #original list
+      list1  = c.user_role_list(tenantID, userID)
+    #add role
+      add    = c.user_role_add(tenantID, userID, roleID)
+      list2  = c.user_role_list(tenantID, userID)
+    #remove role
+      remove = c.user_role_remove(tenantID, userID, roleID)
+      list3  = c.user_role_list(tenantID, userID)
+    #testing add
+      results[0] = add[0] # only user_role_add could make this false
+      results[1] = (list1[1] != list2[1])
+      #results[2] = (c.role_list.key? roleID)
+    #testing remove
+      results[3] = remove[0] # only user_role_remove could make this false
+      results[4] = (list2[1] != list3[1]) && (list1[1] == list3[1])
+      #results[5] = !(c.role_get(roleID).key? userID)
+    #testing list
+      results[6] = list1[0] # only user_role_list could make this false
+      results[7] = list2[0] && list3[0]
+      results[8] = (list1[1] != list2[1]) && (list2[1] != list3[1]) && (list1[1] == list3[1])
+           
+    return results      
+  end
+
+  def setup_user_role_test2(tenantID, userID, role1, role2)
+    c = Keystone.new($admin, $adminPass, $serverURL, $serverPort1, $serverPort2)
+    c.auth($tenant)  
+    results = Array.new(7,true)
+    #original list
+      list0 = c.user_role_list(tenantID, userID)
+    #add first role to user
+      add1  = c.user_role_add(tenantID, userID, role1)
+      list1 = c.user_role_list(tenantID, userID)
+    #add second role to user
+      add2  = c.user_role_add(tenantID, userID, role2)
+      list2 = c.user_role_list(tenantID, userID)
+    #remove first role from user
+      remove1  = c.user_role_remove(tenantID, userID, role1)
+      newlist1 = c.user_role_list(tenantID, userID)
+    #remove second role from user
+      remove2  = c.user_role_remove(tenantID, userID, role2)
+      newlist2 = c.user_role_list(tenantID, userID)    
+    #testing add
+      results[0] = (add1[0] && add2[0])
+      #results[1] = !(list0[1] == list1[1] == list2[1]) #!!!!!!!!!!!!!!!!!!
+      #results[2] = (c.role_list.key? role1) && (c.role_list.key? role2)
+    #testing remove
+      results[3] = (remove1[0] && remove2[0])
+      results[4] = (newlist1[1] != newlist2[1])
+      #results[5] = !((c.role_get(role1).key? userID) && (c.role_get(role2).key? userID))
+    #testing list
+      results[6] = list0[0] && list1[0] && list2[0] && newlist1[0] && newlist2[0]
+      #results[7] = (list0[1] != list1[1] != list2[1]) #!!!!!!!!!!!!!!!!!!!!!!
+      results[8] = (newlist1[1] != newlist2[1])
+    
+    return results      
+  end
+
+  def setup_user_role_test3(tenantID, user1, user2, roleID)
+    c = Keystone.new($admin, $adminPass, $serverURL, $serverPort1, $serverPort2)
+    c.auth($tenant)  
+    results = Array.new(7,true)
+    #original lists
+      user1list0 = c.user_role_list(tenantID, user1)
+      user2list0 = c.user_role_list(tenantID, user2)
+    #add role to first user
+      user1add  = c.user_role_add(tenantID, user1, roleID)
+      user1list1 = c.user_role_list(tenantID, user1)
+    #add role to second user
+      user2add  = c.user_role_add(tenantID, user2, roleID)
+      user2list1 = c.user_role_list(tenantID, user2)
+    #remove role from first user
+      user1remove = c.user_role_remove(tenantID, user1, roleID)
+      user1list2   = c.user_role_list(tenantID, user1)
+    #remove role from second user
+      user2remove = c.user_role_remove(tenantID, user1, roleID)
+      user2list2   = c.user_role_list(tenantID, user1)   
+    #testing add
+      results[0] = (user1add[0] && user2add[0])
+      results[1] = (user1list0 != user1list1) && (user2list0 != user2list1)
+      #results[2] = (c.role_list.key? roleID)
+    #testing remove
+      results[3] = (user1remove[0] && user2remove[0])
+      results[4] = (user1list1[1] != user1list2[1]) && (user2list1[1] != user2list2[1]) 
+      #results[5] = !((c.role_get(roleID).key? user1) && (c.role_get(roleID).key? user2))
+    #testing list
+      results[6] = (user1list0[0] && user1list1[0] && user1list2[0])
+      results[7] = (user2list0[0] && user2list1[0] && user2list2[0])
+      #results[8] = (results[0][1] && results[1][1])
+    
+    return results      
+  end
+  
+  def setup_user_role_test4(tenantID, user1, user2, role1, role2)
+    c = Keystone.new($admin, $adminPass, $serverURL, $serverPort1, $serverPort2)
+    c.auth($tenant)  
+    results = Array.new(3,true)
+    #original lists
+      user1list0 = c.user_role_list(tenantID, user1)
+      user2list0 = c.user_role_list(tenantID, user2)
+      
+    #add first role to first user
+      add1  = c.user_role_add(tenantID, user1, role1)
+      list1 = c.user_role_list(tenantID, user1)
+    #add second role to first user
+      add2  = c.user_role_add(tenantID, user1, role2)
+      list2 = c.user_role_list(tenantID, user1)
+      add_success_user1  = (add1[0] && add2[0])
+      #list_success_user1 = (user1list0[1] != list1[1] != list2[1]) #!!!!!!!!!!!!!
+      
+    #add first role to second user
+      add1  = c.user_role_add(tenantID, user2, role1)
+      list1 = c.user_role_list(tenantID, user2)
+    #add second role to second user
+      add2  = c.user_role_add(tenantID, user2, role2)
+      list2 = c.user_role_list(tenantID, user2)
+      add_success_user2  = (add1[0] && add2[0])
+      #list_success_user2 = (user2list0[1] != list1[1] != list2[1]) #!!!!!!!!!!!!!!!!!!!!!
+          
+    #remove first role from first user
+      remove1 = c.user_role_remove(tenantID, user1, role1)
+      list1   = c.user_role_list(tenantID, user1)
+    #remove second role from first user
+      remove2 = c.user_role_remove(tenantID, user1, role2)
+      list2   = c.user_role_list(tenantID, user1)
+      remove_success_user1 = (remove1[0] && remove2[0])
+      list_success_user1   = (list_success_user1) && (list1 != list2)    
+ 
+    #remove first role from second user
+      remove1 = c.user_role_remove(tenantID, user2, role1)
+      list1   = c.user_role_list(tenantID, user2)
+    #remove second role from second user
+      remove2 = c.user_role_remove(tenantID, user2, role2)
+      list2   = c.user_role_list(tenantID, user2)
+      remove_success_user2 = (remove1[0] && remove2[0])
+      list_success_user2   = (list_success_user2) && (list1 != list2)       
+ 
+    #testing add
+      results[0] = add_success_user1 && add_success_user2
+    #testing remove
+      results[1] = remove_success_user1 && remove_success_user2
+    #testing list
+      results[2] = list_success_user1 && list_success_user2
+    
+    return results      
+  end
+=end
 end
 
